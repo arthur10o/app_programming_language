4f8962aa45773fcfa1a5d63dde7c2ca67a6d9687
arthur10o <arthur.pruvost@protonmail.com>
2025-08-03 17:46:55 +0200

    - Syntactic coloring added for variable name and constant name

--- electron-app/src/scripts/syntax_highlighting.js
+++ electron-app/src/scripts/syntax_highlighting.js
@@ -6,6 +6,9 @@
     Created     : 2025-07-27
     Last Update : 2025-07-27
 */
+
+let var_const_list = [];
+
 function syntax_highlighting() {
     const PREVIEW = document.getElementById("code-editor");
     const CODES_LINES = Array.from(PREVIEW.querySelectorAll('.code-line'));
@@ -71,16 +74,19 @@ function syntax_highlighting() {
                             const typeSpan = `<span class="hl-native-type">${type}</span>`;
                             const nameClass = decl === "const" ? "hl-constant" : "hl-variable";
                             const nameSpan = `<span class="${nameClass}">${name}</span>`;
+                            var_const_list.push(name);
                             return `${declSpan} ${typeSpan} ${nameSpan}`;
                         }
                     },
+                    { regex: /\b[a-zA-Z_$][\w$]*\s*\([^)]*\)\s*{/g, className: "hl-function-method" },
+                    { regex: /\b([a-zA-Z_$][\w$]*)\s*(?=\()/g, className: "hl-function-call" },
                     { regex: /\bimport\s+(?:[\w*\s{},]*\s+from\s+)?["'][^"']+["']/g, className: "hl-import" },
                     { regex: /\btrue|false|null|\d+(\.\d+)?\b/g, className: "hl-literal" },
-                    { regex: /\b[a-zA-Z_$][\w$]*\s*\([^)]*\)\s*{/g, className: "hl-function-methode" },
                     { regex: /\b(const|let|if|else\s+if|else|for|while|return|switch|case|break|default|try|catch|finally|fn)\b/g, className: "hl-keyword" },
                     { regex: /\b(int|float|bool|str|None)\b/g, className: "hl-native-type" },
                     { regex: /\b(if|else if|else|for|while|do|switch|case|default|break|continue|return|try|catch|finally)\b/g, className: "hl-control-structure" }
                 ];
+
 
                 
                 for (const pattern of PATTERNS) {
@@ -93,6 +99,11 @@ function syntax_highlighting() {
 
                 for (const { token, html } of PLACEHOLDERS) {
                     code = code.replace(token, html);
+                }
+                
+                for (const varName of var_const_list) {
+                    const regex = new RegExp(`\\b${varName}\\b`, 'g');
+                    code = code.replace(regex, `<span class="hl-variable">${varName}</span>`);
                 }
 
                 CODE_LINE.innerHTML = code;
