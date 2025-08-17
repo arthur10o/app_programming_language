d52e7357cc622340630a15e30c67f4fa1fbaf639
arthur10o <arthur.pruvost@protonmail.com>
2025-08-18 01:11:25 +0200

    - Creation of the Rust script to hash the password using the Argon2id algorithm during registration - Creation of the Rust script to verify a password using a hash stored using the Argon2id algorithm during loading - [TODO] Integrating scripts into the application

---
+++ electron-app/src/rust/verify_password/verify_password.rs
@@ -0,0 +1,20 @@
+/*
+    FIle        : verify_password.rs
+    Version     : 1.0
+    Description : Function to verify a password with a stored hash using the Argon2id algorithm
+    Author      : Arthur
+    Created     : 2025-08-18
+    Last Update : 2025-08-18
+*/
+use argon2::{Argon2, PasswordHasher, PasswordVerifier, password_hash::{SaltString, PasswordHash}, Params, Algorithm, Version};
+use rand::rngs::OsRng;
+
+fn verify_password(_stored_hash: &str, _password: &str) -> bool {
+    let parsed_hash =  PasswordHash::new(_stored_hash).expect("Failed to parse password hash");
+    let params = Params::new(262144, 6, 2, Some(64)).expect("Invalid Argon2 parameters");
+    let argon2 = Argon2::new(Algorithm::Argon2id, Version::V0x13, params);
+    match argon2.verify_password(_password.as_bytes(), &parsed_hash) {
+        Ok(_) => true,
+        Err(_) => false,
+    }
+}
